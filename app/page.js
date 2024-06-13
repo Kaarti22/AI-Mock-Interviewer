aff9a6bbcd28c41bdea67f76f314da8fbdd3994a
Kaarti22 <kaartimondi@gmail.com>
2024-06-13 18:02:47 +0530

    final commits

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+    <h1>hello world</h1>
+    <Button>Subscribe</Button>
+
+  </div>
+  );
+}
