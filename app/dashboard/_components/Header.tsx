92b6f44fd72b542b25e357b12ef08bcf820b33df
Kaarti22 <kaartimondi@gmail.com>
2024-06-17 15:44:27 +0530

    completed header

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,28 +1,36 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect } from "react";
 import Image from "next/image";
-import {UserButton} from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
-  return (
-    <div className='flex p-4 items-center justify-between'>
-        <Image src={"/logo.png"} width={250} height={150} alt='logo'/>
-        <ul className='flex gap-6'>
-            <li>
-                Dashboard
-            </li>
-            <li>
-                Questions
-            </li>
-            <li>
-                Upgrade
-            </li>
-            <li>
-                How it Works ?
-            </li>
-        </ul>
-        <UserButton/>
-    </div>
-  )
-}
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path);
+  }, []);
 
-export default Header
+  return (
+    <div className="flex p-2 items-center justify-between bg-secondary shadow-sm">
+      <Image src={"/logo.png"} width={250} height={150} alt="logo" />
+      <ul className="hidden md:flex gap-6">
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path === '/dashboard' && 'text-primary font-bold'}`}>
+          Dashboard
+        </li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path === '/dashboard/questions' && 'text-primary font-bold'}`}>
+          Questions
+        </li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path === '/dashboard/upgrade' && 'text-primary font-bold'}`}>
+          Upgrade
+        </li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path === '/dashboard/how' && 'text-primary font-bold'}`}>
+          How it Works ?
+        </li>
+      </ul>
+      <UserButton />
+    </div>
+  );
+};
+
+export default Header;
