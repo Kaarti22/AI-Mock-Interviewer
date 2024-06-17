daec02f8aafa349f4ab7d16004957cd64f8fc7dd
Kaarti22 <kaartimondi@gmail.com>
2024-06-17 16:01:42 +0530

    setup drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'drizzle-kit'
+export default defineConfig({
+ schema: "./schema.ts",
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: 'postgresql://neondb_owner:P4sVkcXI9DHe@ep-white-band-a1j2saxg.ap-southeast-1.aws.neon.tech/AI%20mock%20Interviewer?sslmode=require',
+  },
+  verbose: true,
+  strict: true,
+})
