33234af175338c34baeb71d61f87d78e828f9522
Kaarti22 <kaartimondi@gmail.com>
2024-06-17 20:04:47 +0530

    finished interview landing page

--- app/dashboard/interview/[interviewId]/page.tsx
+++ app/dashboard/interview/[interviewId]/page.tsx
@@ -5,8 +5,8 @@ import { db } from "../../../../utils/db";
 import { MockInterview } from "../../../../utils/schema";
 import { eq } from "drizzle-orm";
 import Webcam from "react-webcam";
-import { WebcamIcon } from "lucide-react";
-import {Button} from "@/components/ui/button";
+import { Lightbulb, WebcamIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const Interview = ({ params }) => {
   const [interviewData, setInterviewData] = useState({});
@@ -28,19 +28,59 @@ const Interview = ({ params }) => {
   };
 
   return (
-    <div className="my-10 flex justify-center flex-col items-center">
+    <div className="my-10">
       <h2 className="font-bold text-2xl">Let's Get Started</h2>
-      <div>
-        {webCamEnabled ? 
-          <Webcam
-            onUserMedia={() => setWebCamEnabled(true)} onUserMediaError={() => setWebCamEnabled(false)} mirrored={true} style={{ height: 300, width: 300}}
-          />
-         : 
-          <>
-            <WebcamIcon className="h-72 w-full my-7 p-20 bg-secondary rounded-lg border" />
-            <Button onClick={() => setWebCamEnabled(true)}>Enable Web Cam and Microphone</Button>
-          </>
-        }
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="flex flex-col my-5 gap-5">
+          <div className="flex flex-col p-5 rounded-lg border gap-5">
+            <h2>
+              <strong>Job Role/Job Position: </strong>{" "}
+              {interviewData.jobPosition}
+            </h2>
+            <h2>
+              <strong>Job Description/Tech Stack: </strong>{" "}
+              {interviewData.jobDesc}
+            </h2>
+            <h2>
+              <strong>Years of Experience: </strong>{" "}
+              {interviewData.jobExperience}
+            </h2>
+          </div>
+          <div className="p-5 border rounded-lg border-yellow-300 bg-yellow-100">
+            <h2 className="flex items-center gap-2 text-yellow-500">
+              <Lightbulb />
+              <strong>Information</strong>
+            </h2>
+            <h2 className="mt-3 text-yellow-500">
+              Enable Video, Web cam and Microphone to start your AI generated
+              mock interview. It has 10 questions which you can answer and at
+              the last you will get the report on the basis of your answers.<br/>
+              <b>NOTE: </b> We never revord your video. You can disable your web cam at
+              any time. It is only kept to build your confidence in the
+              interviews.
+            </h2>
+          </div>
+        </div>
+        <div>
+          {webCamEnabled ? (
+            <Webcam
+              onUserMedia={() => setWebCamEnabled(true)}
+              onUserMediaError={() => setWebCamEnabled(false)}
+              mirrored={true}
+              style={{ height: 300, width: 300 }}
+            />
+          ) : (
+            <div className="flex flex-col">
+              <WebcamIcon className="h-72 w-full my-7 p-20 bg-secondary rounded-lg border" />
+              <Button variant={"ghost"} onClick={() => setWebCamEnabled(true)}>
+                Enable Web Cam and Microphone
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="flex justify-end items-end">
+        <Button>Start Interview</Button>
       </div>
     </div>
   );
