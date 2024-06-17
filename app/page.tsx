f7e236ba9a7194d62a68b6d860da383e83cb7fcf
Kaarti22 <kaartimondi@gmail.com>
2024-06-17 12:38:28 +0530

    installed button component from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import React from "react";
+import { Button } from "../components/ui/button";
 
 export default function Home() {
   return (
     <div>
       Hello world
+      <Button>Subscribe</Button>
     </div>
   );
 }
