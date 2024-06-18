16b757e395a2a520dcdc1764b7ea6d999cea3637
Kaarti22 <kaartimondi@gmail.com>
2024-06-19 00:30:54 +0530

    minor CSS fix

--- app/dashboard/_components/InterviewList.tsx
+++ app/dashboard/_components/InterviewList.tsx
@@ -30,7 +30,7 @@ const InterviewList = () => {
 
   return (
     <div>
-      <h2 className="font-medium font-semibold text-xl text-blue-700">Previous Mock Interviews</h2>
+      <h2 className="font-medium text-xl text-blue-700">Previous Mock Interviews</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-3">
         {interviewList && interviewList.map((interview, index) => (
             <InterviewItemCard key={index} interview={interview}/>
