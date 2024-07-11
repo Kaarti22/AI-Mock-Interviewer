8d95016ba90209cd0a73bfdcbeee65f25cec09d4
Kaarti22 <kaartimondi@gmail.com>
2024-07-11 23:05:28 +0530

    fixed layout problem

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Header from './_components/Header';
+import React from "react";
+import Header from "./_components/Header";
 
-const DashboardLayout = ({children}) => {
+const DashboardLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
   return (
     <div>
-        <Header/>
-        <div className='mx-5 md:mx-20 lg:mx-36'>
-          {children}
-        </div>
+      <Header />
+      <div className="mx-5 md:mx-20 lg:mx-36">{children}</div>
     </div>
-  )
-}
+  );
+};
 
 export default DashboardLayout;
